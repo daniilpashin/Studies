9f9e27477cd442f773599d5c248bc3b48559f5a3
Daniil Pashin <danypashin@yandex.ru>
2019-11-29 14:24:52 +0300

    [+] Add lab 4 to lab 2&3

---
+++ 2 course/Laboratory2/Laboratory2/CFour.cpp
@@ -0,0 +1,9 @@
+//
+//  CFour.cpp
+//  Laboratory2
+//
+//  Created by Даниил on 29/11/2019.
+//  Copyright © 2019 shad0wdev. All rights reserved.
+//
+
+#include "CFour.hpp"
---
+++ 2 course/Laboratory2/Laboratory2/CFour.hpp
@@ -0,0 +1,14 @@
+//
+//  CFour.hpp
+//  Laboratory2
+//
+//  Created by Даниил on 29/11/2019.
+//  Copyright © 2019 shad0wdev. All rights reserved.
+//
+
+#ifndef CFour_hpp
+#define CFour_hpp
+
+#include <stdio.h>
+
+#endif /* CFour_hpp */
